6fc210f0e7d488b5706c2d68581bf8185d280829
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-09-28 13:37:52 +0200

    loadmore

--- src/01-gallery.js
+++ src/01-gallery.js
@@ -8,6 +8,7 @@ const form = document.querySelector('#form');
 const input = document.querySelector('#form-input');
 const loader = document.querySelector('#loader');
 const gallery = document.querySelector('#gallery');
+const moreBtn = document.querySelector('#more');
 
 let searchString = '';
 const APIURL = `https://pixabay.com/api/`;
@@ -113,44 +114,59 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   gallery.innerHTML = '';
   searchString = input.value.trim();
-  const params = new URLSearchParams({
-    key: '35169635-92091552d9eccdba3eb57d7a9',
-    q: searchString,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-    per_page: 39,
-  });
+  let pageIndex = 0;
 
+  const getPics = async () => {
+    try {
+      pageIndex++;
+      const params = new URLSearchParams({
+        key: '35169635-92091552d9eccdba3eb57d7a9',
+        q: searchString,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: pageIndex,
+        per_page: 39,
+      });
+      const response = (await axios.get(`${APIURL}?${params}`)).data.hits;
+      return response;
+    } catch (error) {
+      console.log(error);
+      iziToast.error({
+        message: 'Oh no! There is an error: ' + error,
+      });
+    }
+  };
   if (searchString !== '') {
     loader.classList.remove('visibility');
 
-    const getPics = async () => {
-      try {
-        return await axios.get(`${APIURL}?${params}`);
-      } catch (error) {
-        console.log(error);
-        iziToast.error({
-          message: 'Oh no! There is an error: ' + error,
-        });
-      }
-    };
-    getPics().then(response => {
+    getPics().then(hits => {
       loader.classList.add('visibility');
-      const hits = response.data.hits;
       input.value = '';
+
+      renderElements(hits, gallery);
+      lightbox.refresh();
+      moreBtn.classList.remove('visibility');
       if (hits.length === 0) {
         iziToast.error({
           message:
             'Sorry, there are no images matching your search query. Please try again!',
         });
+        moreBtn.classList.add('visibility');
       }
-      renderElements(hits, gallery);
-      lightbox.refresh();
+    });
+    moreBtn.addEventListener('click', e => {
+      loader.classList.remove('visibility');
+      getPics().then(hits => {
+        loader.classList.add('visibility');
+        renderElements(hits, gallery);
+        lightbox.refresh();
+      });
     });
   } else {
     iziToast.info({
       message: 'Type correct search params!',
     });
+    moreBtn.classList.add('visibility');
   }
 });
